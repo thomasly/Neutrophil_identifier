52808c8b70c6828f6ab516892c9a9476f9462c4c
Yang Liu <thomasliuy@gmail.com>
2018-07-18 15:09:22 -0400

    load_data.py ver0.1 able to read data and return X_train, X_test, Y_train, Y_test now

--- ResNet/load_data.py
+++ ResNet/load_data.py
@@ -12,17 +12,31 @@ from PIL import Image
 
 
 def load_data():
+	"""
+	load pil image data into numpy arrays
+
+	input:
+	pil images in pos/neg folders
+
+	output:
+	training data as X_train_orig
+	training data labels as Y_train_orig
+	test data as X_test_orig
+	test data labels as Y_test_orig
+	"""
 
 	pos_path = None
 	neg_path = None
 	pos_img = []
 	neg_img = []
 
+	# paths to positive/negative images
 	if os.path.exists('../pos'):
 		pos_path = os.path.abspath('../pos')
 	if os.path.exists('../neg'):
 		neg_path = os.path.abspath('../neg') 
 
+	# open positive/negative images and transform them into np arrays
 	for file in os.listdir(pos_path):
 		img_path = pos_path + os.sep + str(file)
 		img = Image.open(str(img_path))
@@ -34,11 +48,43 @@ def load_data():
 		img = Image.open(str(img_path))
 		neg_img.append(np.array(img))
 
-
+	# remove the transparency channel
+	# add labels to the images
 	pos_img = np.array(pos_img)[:, :, :, 0:3]
+	pos_label = np.ones(pos_img.shape[0])
 	neg_img = np.array(neg_img)[:, :, :, 0:3]
+	neg_label = np.zeros(neg_img.shape[0])
 
-	return pos_img, neg_img
+	# combine positive and negative datasets
+	X = np.concatenate((pos_img, neg_img), axis=0)
+	Y = np.concatenate((pos_label, neg_label), axis=0)
+	
+	# number of samples
+	n_X = X.shape[0]
+
+	# shuffle the combined datasets
+	perm = np.random.permutation(n_X)
+	X = X[perm]
+	Y = Y[perm]
+
+	# determine the images number of test set
+	n_test = np.int(n_X / 10) * 2
+	# if test set number is less than 1 (total data number is less than 10)
+	# add 1 to test set
+	if n_test == 0:
+		n_test += 1
+
+	# determine the number of training set
+	n_train = n_X - n_test
+	
+	# split data into training set and test set
+	X_train_orig = X[0:n_train]
+	X_test_orig = X[-n_test:]
+	Y_train_orig = Y[0:n_train]
+	Y_test_orig = Y[-n_test:]
+	
+
+	return X_train_orig, X_test_orig, Y_train_orig, Y_test_orig
 
 
 if __name__ == "__main__":
