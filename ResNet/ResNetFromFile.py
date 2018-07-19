3315eb5ff3b514b38c7fb5d55aeb963084142730
Yang Liu <thomasliuy@gmail.com>
2018-07-19 11:06:11 -0400

    fix bugs

--- ResNet/ResNetFromFile.py
+++ ResNet/ResNetFromFile.py
@@ -8,6 +8,8 @@
 
 from keras.models import model_from_json
 import os
+import load_data as ld
+from numpy import squeeze
 
 def load_model_from_json(model_path=None, weights_path=None):
 
@@ -33,9 +35,12 @@ def test_loaded_model():
 	model = load_model_from_json()
 	model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 	_, X_test, _, Y_test = ld.load_data(500, 500)
-	preds = model.evaluate(X_test, Y_test, )
-	print ("Loss = " + str(preds[0]))
-    print ("Test Accuracy = " + str(preds[1]))
+	X_test = X_test / 255
+	Y_test = squeeze(Y_test)
+
+	preds = model.evaluate(X_test, Y_test)
+	print("Loss = " + str(preds[0]))
+	print("Test Accuracy = " + str(preds[1]))
 
 
 if __name__ == '__main__':
